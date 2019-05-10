bb3c74b19a44e282336e551f41c9605cd1b04acd
Simen Sverdrup-Thygeson <simenthygeson@gmail.com>
2019-05-10 11:02:08 +0900

    push2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*******************\nWelcome to GOMOKU!\n*******************");
-        
+        Scanner input = new Scanner(System.in);
+        String userChoice = "";
+        while (userChoice != "q") {
+            System.out.printf("*******************\nWelcome to GOMOKU!\n*******************");
+            while (!(userChoice.equals("s") || userChoice.equals("m") || userChoice.equals("r") || userChoice.equals("q"))) {
+                System.out.println("Please choose Singleplayer (s), Multiplayer (m), Ranking (r) or Quit (q): ");
+                userChoice = input.nextLine();
+            }
+
+            switch (userChoice) {
+                case "s":
+                    Singleplayer single = new Singleplayer();
+                    single.play();
+                case "m":
+                    Multiplayer multi = new Multiplayer();
+                    multi.play();
+                case "r":
+                    Ranking ranking = new Ranking();
+                    ranking.seeRanking();
+                case "q":
+                    continue;
+            }
+        }
     }
 }
